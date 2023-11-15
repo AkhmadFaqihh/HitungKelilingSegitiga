bbe2501c66c35e0c421d3eb9d67190eeffb2cd4a
AkhmadFaqihh <akhmad.faqih.ft20@mail.umy.ac.id>
2023-11-15 15:06:23 +0700

    Add project files.

---
+++ HitungKelilingSegitiga/HitungKeliling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NUnitTest
+{
+    public class HitungKeliling
+    {
+        public int HKSegitiga(int s1, int s2, int s3)
+        {
+            return s1 + s2 + s3;
+        }
+    }
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the HitungKeliling class
+            HitungKeliling hitungKeliling = new HitungKeliling();
+
+            // Call the HKSegitiga method with sample values
+            int hasilKeliling = hitungKeliling.HKSegitiga(3, 4, 5);
+
+            // Display the result
+            Console.WriteLine("Hasil Keliling Segitiga: " + hasilKeliling);
+        }
+    }
+}
---
+++ HitungKelilingSegitigaTests/HitungKelilingTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NUnitTest.Tests
+{
+    [TestClass()]
+    public class HitungKelilingTests
+    {
+        [TestMethod()]
+        public void HKSegitigaTest()
+        {
+            HitungKeliling HK = new HitungKeliling();
+            Assert.AreEqual(30, HK.HKSegitiga(10, 10, 10));
+        }
+    }
+}
